b9021d825ba841a33cf8eed4d46373cfbb0a5361
emuddled <emuddled@gmail.com>
2020-08-30 14:34:11 -0700

    Add files via upload
    Trying to figure out git

---
+++ Part2/Program.cs
@@ -0,0 +1,45 @@
+﻿/// Chapter No. 1		Exercise No. 2
+/// File Name: SimpleDrawWizard
+/// @author: Evan Sinclair
+/// Date:  August 24, 2020
+///
+/// Problem Statement: Ask the user to enter two numbers, calculate the sum of
+/// these two numbers, and display the sum to the screen
+/// 
+/// 
+/// Overall Plan:
+/// 1) Print an initial welcoming message to the screen
+/// 2) Prompt the user for two integers
+/// 3) Calculate the sum of the integers
+/// 4) Print the sum of the integers to the screen
+/// 
+using System;
+
+namespace Part2
+{
+    class SimpleDrawWizard
+    {
+        static void Main(string[] args)
+        {
+            // print a message to the screen
+            Console.WriteLine("                     ***                    ");
+            Console.WriteLine("                    ****                    ");
+            Console.WriteLine("                  *****                     ");
+            Console.WriteLine("                 *******                    ");
+            Console.WriteLine("                 *****               *      ");
+            Console.WriteLine("                 ********           **      ");
+            Console.WriteLine("       *        ***********        ***      ");
+            Console.WriteLine("      ***     *************        ***      ");
+            Console.WriteLine("     **********************************     ");
+            Console.WriteLine("    *********************************       ");
+            Console.WriteLine("                *   *  *   *                ");
+            Console.WriteLine("                *         *                 ");
+            Console.WriteLine("                 **  **  *                  ");
+            Console.WriteLine("                   ******                   ");
+            Console.WriteLine("                    *****                   ");
+            Console.WriteLine("                     ***                    ");
+            Console.WriteLine("                     **                     ");
+            Console.WriteLine("                     *                      ");
+        }
+    }
+}
